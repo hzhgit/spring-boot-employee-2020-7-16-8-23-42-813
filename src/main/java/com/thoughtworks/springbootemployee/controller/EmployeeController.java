164f4d80afd44f60878dbe1c16c83a0eb4793d5d
zach-he <971499086@qq.com>
2020-08-02 12:26:23 +0800

    [zach]refactor:add initData method

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -12,15 +12,19 @@ import java.util.List;
 @RequestMapping("/employees")
 public class EmployeeController {
 
-    @GetMapping
-    public List<Employee> getAllEmployees(){
+    private List<Employee> initData() {
         List<Employee> employees = new ArrayList<>();
-        employees.add(new Employee(1,"zach",18,"male"));
-        employees.add(new Employee(2,"karen",18,"female"));
-        employees.add(new Employee(3,"york",19,"male"));
-        employees.add(new Employee(4,"green",19,"male"));
-        employees.add(new Employee(5,"chris",20,"male"));
-        employees.add(new Employee(6,"alex",21,"female"));
-        return employees;
+        employees.add(new Employee(1, "zach", 18, "male"));
+        employees.add(new Employee(2, "karen", 18, "female"));
+        employees.add(new Employee(3, "york", 19, "male"));
+        employees.add(new Employee(4, "green", 19, "male"));
+        employees.add(new Employee(5, "chris", 20, "male"));
+        employees.add(new Employee(6, "alex", 21, "female"));
+        return  employees;
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return initData();
     }
 }
